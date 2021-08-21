c36d7613b4f702d6a406cdc4c94699e0d408a25a
ErPrabin <prabinmaneeshrestha22@gmail.com>
2021-08-21 12:35:11 +0545

    cart and show single item link added in index page

--- resources/views/frontend/partials/todays-special.blade.php
+++ resources/views/frontend/partials/todays-special.blade.php
@@ -17,12 +17,25 @@
                             </div>
                         </div><span class="product-badge product-badge-sale">Sale</span>
                         <div class="product-button-wrap">
-                            <div class="product-button"><a
-                                    class="button button-secondary button-zakaria fl-bigmug-line-search74"
-                                    href="single-product.html"><i class="fas fa-eye"></i></a></div>
-                            <div class="product-button"><a
-                                    class="button button-primary button-zakaria fl-bigmug-line-shopping202"
-                                    href="cart-page.html"><i class="fas fa-shopping-cart"></i></a></div>
+                            <div class="product-button">
+                                <a class="button button-secondary button-zakaria fl-bigmug-line-search74"
+                                    href="{{ route('singleItem', ['id' => $item->id]) }}">
+                                    <i class="fas fa-eye"></i>
+                                </a>
+                            </div>
+                            <div class="product-button">
+                                <form method="post" action="{{ route('cart.store') }}" enctype="multipart/form">
+                                    @csrf
+                                    <input name="id" type="hidden" class="form-control" value="{{ $item->id }}">
+                                    <input name="name" type="hidden" class="form-control" value="{{ $item->name }}">
+                                    <input name="price" type="hidden" class="form-control" value="{{ $item->price }}">
+                                    <input name="image" type="hidden" class="form-control" value="{{ $item->image }}">
+                                    <button type="submit"
+                                        class="button button-primary button-zakaria fl-bigmug-line-shopping202">
+                                        <i class="fas fa-shopping-cart"></i>
+                                    </button>
+                                </form>
+                            </div>
                         </div>
                     </article>
                 </div>
