daea73afdade569b93ed28117f6b3c5b950e7ae0
SantoshiBhujel <santoshibhujel35@gmail.com>
2021-08-23 21:38:35 +0545

    home route added

--- resources/views/profile/show.blade.php
+++ resources/views/profile/show.blade.php
@@ -1,8 +1,10 @@
 <x-app-layout>
     <x-slot name="header">
         <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Profile') }}
+            <a href="{{ route('home') }}">{{ __('Home') }}</a> > 
+            <a href="{{ route('profile') }}">{{ __('Profile') }}</a>            
         </h2>
+        
     </x-slot>
 
     <div>
